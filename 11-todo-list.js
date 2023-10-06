3f7a290141c8a0b788a229059bc453e934fc8ed9
nitish-pardal <nitishpardal@gmail.com>
2023-10-06 18:57:19 +0530

    06-10-2023

---
+++ 11-todo-list.js
@@ -0,0 +1,11 @@
+const todoList=[];//empty array
+
+function addTodo(){
+ const inputElement = document.querySelector('.js-name-input');
+ const name = inputElement.value;
+ todoList.push(name);
+ console.log(todoList);
+
+  inputElement.value = '';
+
+}
